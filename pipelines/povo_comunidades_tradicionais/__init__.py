0a49ea950c9d8642e4a49daf486e325b24f0aea6
KarinaPassos <kaadrain@gmail.com>
2024-02-02 11:43:21 -0300

    migrating smac pipelines

---
+++ pipelines/povo_comunidades_tradicionais/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""
+Prefect flows for Escritorio's project
+"""
+from pipelines.povo_comunidades_tradicionais.materialize_povo_comunidades_tradicionais.flows import *  # noqa
---
+++ pipelines/povo_comunidades_tradicionais/materialize_povo_comunidades_tradicionais/flows.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+DBT-related flows.
+"""
+
+from copy import deepcopy
+
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+
+from pipelines.constants import constants
+from pipelines.povo_comunidades_tradicionais.materialize_povo_comunidades_tradicionais.schedules import (
+    materialize_povo_comunidades_tradicionais_schedule,
+)
+from prefeitura_rio.pipelines_templates.run_dbt_model.flows import templates__run_dbt_model__flow
+
+materialize_povo_comunidades_tradicionais_flow = deepcopy(templates__run_dbt_model__flow)
+materialize_povo_comunidades_tradicionais_flow.name = (
+    "SMAC: povo_comunidades_tradicionais - Materializar tabelas"
+)
+materialize_povo_comunidades_tradicionais_flow.storage = GCS(
+    constants.GCS_FLOWS_BUCKET.value
+)
+materialize_povo_comunidades_tradicionais_flow.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value,
+    labels=[
+        constants.RJ_SMAC_AGENT_LABEL.value,
+    ],
+)
+materialize_povo_comunidades_tradicionais_flow.schedule = (
+    materialize_povo_comunidades_tradicionais_schedule
+)
