ea8516d5ba88b862c804d37c18e825a5b3c5bdb1
d116626 <d116626@gmail.com>
2024-02-05 16:53:35 -0300

    feat: add template

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -2,4 +2,6 @@
 """
 Imports all flows for every project so we can register all of them.
 """
+from pipelines.exemplo import *  # noqa
 from pipelines.povo_comunidades_tradicionais import *  # noqa
+from pipelines.templates import *  # noqa
