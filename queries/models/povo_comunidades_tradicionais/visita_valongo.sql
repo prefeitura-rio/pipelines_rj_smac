d0f03e7661215c46038ce3ba924e7564a97c5e6a
KarinaPassos <kaadrain@gmail.com>
2024-02-07 14:44:44 -0300

    reorganising models

--- queries/models/povo_comunidades_tradicionais/visita_valongo.sql
+++ queries/models/povo_comunidades_tradicionais/visita_valongo.sql
@@ -1,5 +1,7 @@
+{{ config(alias='visita_valongo') }}
+
 SELECT 
-    DATETIME(periodo, "America/Sao_Paulo") AS ano_mes,
+    DATE(periodo) AS ano_mes,
     SAFECAST(ano AS INT64) AS ano,
     CAST(mes AS STRING) AS mes,
     CAST(valor_visitacao AS INT64) AS valor_visitacao
