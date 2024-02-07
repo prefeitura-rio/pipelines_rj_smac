0eb0c5e911243b5d1c31f4e8754b0dbb903edbff
KarinaPassos <kaadrain@gmail.com>
2024-02-07 11:58:08 -0300

    setting dbt models

---
+++ queries/models/povo_comunidades_tradicionais/investimento_valongo.sql
@@ -0,0 +1,14 @@
+{{ config(alias='investimento_valongo') }}
+
+SELECT 
+    CAST(nome_org AS STRING) AS nome_org,
+    CAST(v__nculo_institucional AS STRING) AS vinculo_institucional,
+    CAST(natureza_inv AS STRING) AS natureza_inv,
+    CAST(origem_inv AS STRING) AS origem_inv,
+    CAST(fonte_inv AS STRING) AS fonte_inv,
+    CAST(abrang__ncia_origem_inv AS STRING) AS abrangencia_origem_inv,
+    SAFE_CAST(valor_inv__R__ AS INT64) AS valor_inv,
+    CAST(periodo_inv AS STRING) AS periodo_inv,
+    CAST(objetivo_inv_ AS STRING) AS objetivo_inv,
+    CAST(fonte_dados AS STRING) AS fonte_dados
+FROM `rj-smac.investimento_valongo_staging.investimento_valongo`
---
+++ queries/models/povo_comunidades_tradicionais/visita_valongo.sql
@@ -0,0 +1,6 @@
+SELECT 
+    DATETIME(periodo, "America/Sao_Paulo") AS ano_mes,
+    SAFECAST(ano AS INT64) AS ano,
+    CAST(mes AS STRING) AS mes,
+    CAST(valor_visitacao AS INT64) AS valor_visitacao
+FROM `rj-smac.povo_comunidades_tradicionais_staging.visita_valongo`
